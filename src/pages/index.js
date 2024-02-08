e9cd003167f0b34d4f5cb4681c67922e6a004a67
deshpandeadi1901 <deshpande.aditya@northeastern.edu>
2024-02-07 23:08:44 -0500

    in class work

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,27 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+// import Profile from './components/profile'
+// import Gallery from './components/gallery'
+import Profile from './components/profile_props'
+import MyComp from './qcomps/firstcomp'
+import Bio from './qcomps/bios'
+import TodoList from './qcomps/todos'
+import Gallery from './qcomps/gallery_props'
+import Holder from './components/square'
+import PackingList from './qcomps/props_item'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        {/* <Profile /> */}
+        {/* <MyComp/>
+        <Bio/>
+        <TodoList/>
+        <Profile/> */}
+        <Gallery/> 
+        {/* <Holder/> */}
+        {/* <PackingList/> */}
     </div>
   )
 }

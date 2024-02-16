7b86a43340537f130be79f564c2d5e861d5313da
deshpandeadi1901 <deshpande.aditya@northeastern.edu>
2024-02-16 16:30:52 -0500

    in class work

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,16 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import RequestTracker from './qcomps/shoppingCart'
+import Scoreboard from './qcomps/updObjectsForm'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        {/* <Profile /> */}
+        <Scoreboard/>
     </div>
   )
 }

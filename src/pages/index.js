c38c49dcb898fb553adf8248732efacb915f1500
deshpandeadi1901 <deshpande.aditya@northeastern.edu>
2024-02-21 15:07:53 -0500

    in class work

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,13 @@ import Form from './qcomps/stuckForm'
 import FeedbackForm from './qcomps/thankYouCrash'
 import RequestTracker from './qcomps/shoppingCart'
 import Scoreboard from './qcomps/updObjectsForm'
+import List from './qcomps/artistsRemoveArr'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         {/* <Profile /> */}
-        <Scoreboard/>
+        <List/>
     </div>
   )
 }
